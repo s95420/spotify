ab36ec08ec9c24c32828e0eae473d51bde45d920
s95420 <s95420@pollub.edu.pl>
2023-12-12 00:58:03 +0100

    commit

---
+++ src/main/java/org/example/SpotifyDataRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.bialy.spotifydatarestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyDataRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyDataRestApiApplication.class, args);
+	}
+
+}
