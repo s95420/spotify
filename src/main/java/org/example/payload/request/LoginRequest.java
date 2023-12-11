3f27645146fef47d4d2a653ba4ca5632fe915838
s95420 <s95420@pollub.edu.pl>
2023-12-11 22:18:36 +0100

    new login request

---
+++ src/main/java/org/example/payload/request/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.bialy.spotifydatarestapi.payload.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class LoginRequest {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+}
