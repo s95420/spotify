55e9fb4c8b90606e9f6e0e96400d1d9db58a3a82
s95420 <s95420@pollub.edu.pl>
2023-12-12 00:18:56 +0100

    UserRepository created

---
+++ src/main/java/org/example/repository/UserReposotry.java
@@ -0,0 +1,12 @@
+package com.bialy.spotifydatarestapi.repository;
+
+import com.bialy.spotifydatarestapi.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
