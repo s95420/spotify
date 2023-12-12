5a6fc56d08c7c937e153c64c768b6503795d1f03
s95420 <s95420@pollub.edu.pl>
2023-12-12 01:26:21 +0100

    Top and viral components added

---
+++ .idea/frontend/public/src/components/Viral50GlobalChart.js
@@ -0,0 +1,199 @@
+import React, { useEffect, useState } from "react";
+import Pagination from "@material-ui/lab/Pagination";
+import { Chart } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  BarController,
+  BarElement,
+  PointElement,
+  LinearScale,
+  Title,
+  Tooltip,
+  TimeScale,
+} from "chart.js";
+import { CategoryScale } from "chart.js";
+import AuthService from "../services/auth.service";
+import { toXML } from "jstoxml";
+ChartJS.register(
+  PointElement,
+  LinearScale,
+  Title,
+  Tooltip,
+  TimeScale,
+  BarController,
+  BarElement,
+  CategoryScale
+);
+const currentUser = AuthService.getCurrentUser();
+export const options = {
+  plugins: {
+    title: {
+      display: true,
+      text: "Popularność artystów w kategorii Viral50 - cały świat",
+      font: {
+        size: 15,
+        weight: "bold",
+      },
+    },
+  },
+  scales: {
+    x: {
+      ticks: {
+        maxRotation: 90,
+        minRotation: 60,
+        autoSkip: false,
+        font: {
+          size: 10,
+        },
+      },
+      title: {
+        display: true,
+        text: "Nazwa artysty/ów i tytuł utworu",
+        font: {
+          size: 15,
+          weight: "bold",
+        },
+      },
+    },
+    y: {
+      title: {
+        display: true,
+        text: "Pozycja w rankingu",
+        font: {
+          size: 15,
+          weight: "bold",
+        },
+      },
+    },
+  },
+};
+
+export const Viral50GlobalChart = () => {
+  const [content, setContent] = useState([]);
+  const [page, setPage] = useState(1);
+  const [count, setCount] = useState(0);
+  const xLabels = [];
+  const yLabels = [];
+  const apiURL = `http://localhost:8080/api/v1/viral50/Global?page=${page}&sortDate=DESC&sortRank=ASC`;
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(apiURL, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${currentUser.token}`,
+        },
+      });
+      const json = await response.json();
+      setCount(json.totalPages);
+      setContent(json.content);
+    })();
+  }, [apiURL]);
+
+  const fetchDataChart = async () => {
+    const response = await fetch(apiURL, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${currentUser.token}`,
+      },
+    });
+    //console.log(currentUser.token)
+    const json = await response.json();
+    setCount(json.totalPages);
+    setContent(json.content);
+  };
+
+  content &&
+    content.forEach((v) => {
+      xLabels.push(v.artist + " - " + v.title);
+      yLabels.push(v.rank);
+    });
+
+  const handlePageChange = (event, value) => {
+    setPage(value);
+    fetchDataChart();
+  };
+  const JsonFile = () => {
+    const element = document.createElement("a");
+    const saveFile = JSON.stringify(content);
+    const textFile = new Blob([saveFile], { type: "application/json" }); //pass data from localStorage API to blob
+    element.href = URL.createObjectURL(textFile);
+    element.download = "file.json";
+    document.body.appendChild(element);
+    element.click();
+  };
+
+  const XmlFile = () => {
+    const xmlOptions = {
+      header: true,
+      indent: "  ",
+    };
+    const element = document.createElement("a");
+    const saveFile = toXML(
+      {
+        _name: "viral50Global",
+        _attrs: {
+          version: "1.0",
+        },
+        _content: {
+          records: [content],
+        },
+      },
+      xmlOptions
+    );
+    const textFile = new Blob([saveFile], { type: "application/xml" }); //pass data from localStorage API to blob
+    element.href = URL.createObjectURL(textFile);
+    element.download = "file.xml";
+    document.body.appendChild(element);
+    element.click();
+  };
+  return (
+    <div>
+      <div className="container bg-light">
+        <Chart
+          height={700}
+          width={1500}
+          options={options}
+          type="bar"
+          data={{
+            labels: xLabels,
+            datasets: [
+              {
+                label: "Pozycja w rankingu:",
+                backgroundColor: [
+                  "#3e95cd",
+                  "#8e5ea2",
+                  "#3cba9f",
+                  "#e8c3b9",
+                  "#c45850",
+                  "#CD5C5C",
+                  "#40E0D0",
+                ],
+                data: yLabels,
+              },
+            ],
+          }}
+        />
+      </div>
+      <div className="mt-5 col-md-12 text-center">
+        <Pagination
+          className="my-3 d-flex justify-content-center"
+          count={count}
+          page={page}
+          siblingCount={1}
+          boundaryCount={1}
+          variant="outlined"
+          shape="rounded"
+          onChange={handlePageChange}
+        />
+        <button className="btn btn-primary mr-2" onClick={JsonFile}>
+          Zapisz do pliku JSON
+        </button>
+        <button className="btn btn-primary" onClick={XmlFile}>
+          Zapisz do pliku XML
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Viral50GlobalChart;
